a37a68a98283224394462aa4852041504b508d3c
Buster "Silver Eagle" Neece <buster@busterneece.com>
2014-09-19 18:33:48 -0500

    Wiring up song likes and dislikes.

--- pvlive/www/js/controllers.js
+++ pvlive/www/js/controllers.js
@@ -1,10 +1,13 @@
 var controllers = angular.module('pvlive.controllers', []);
 
-controllers.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+controllers.controller('AppCtrl', function($scope) {
 
 });
 
-controllers.controller('StationsCtrl', function($scope, pvlService, $ionicModal, $timeout) {
+/**
+ * Stations Listing
+ */
+controllers.controller('StationsCtrl', function($scope, pvlService, $timeout) {
 
     var np_timeout = null;
 
@@ -45,35 +48,84 @@ controllers.controller('StationsCtrl', function($scope, pvlService, $ionicModal,
 
 });
 
-controllers.controller('StationCtrl', function($scope, $stateParams, pvlService, $ionicModal, $timeout) {
+/**
+ * Individual Station & Playback
+ */
+controllers.controller('StationCtrl', function($scope, $stateParams, pvlService, $timeout, $window) {
 
     var np_timeout = null;
     var active_stream = null;
+    var playing_stream = null;
+    var audio_element = null;
 
     $scope.station = {};
     $scope.stream = {};
 
     $scope.reloadPage = function() { loadNowPlaying() };
+
+    // On-page conditionals.
     $scope.isActiveStream = function(stream) {
         return (active_stream == stream.id);
     };
+    $scope.isPlaying = function(stream) {
+        return (active_stream == stream.id && playing_stream == stream.id);
+    };
 
+    // On page triggered events.
     $scope.playStream = function(stream)
     {
         active_stream = stream.id;
 
         processNowPlaying();
+
+        // Special handling for video streams.
+        if (stream.type == "stream")
+        {
+            $window.open(stream.url);
+            return;
+        }
+
+        if ($scope.isPlaying(stream))
+            stopPlayer();
+        else
+            playStream($scope.stream);
     };
 
     $scope.likeSong = function(song)
     {
+        if ($('#btn-like-song').hasClass('positive'))
+        {
+            pvlService.voteClear(song.sh_id);
 
+            $('#btn-like-song,#btn-dislike-song').removeClass('positive assertive');
+        }
+        else
+        {
+            pvlService.voteLike(song.sh_id);
+
+            $('#btn-like-song,#btn-dislike-song').removeClass('positive assertive');
+            $('#btn-like-song').addClass('positive');
+        }
     };
+
     $scope.dislikeSong = function(song)
     {
+        if ($('#btn-dislike-song').hasClass('assertive'))
+        {
+            pvlService.voteClear(song.sh_id);
 
+            $('#btn-like-song,#btn-dislike-song').removeClass('positive assertive');
+        }
+        else
+        {
+            pvlService.voteDislike(song.sh_id);
+
+            $('#btn-like-song,#btn-dislike-song').removeClass('positive assertive');
+            $('#btn-dislike-song').addClass('assertive');
+        }
     };
 
+    // Now-playing processing.
     loadNowPlaying();
 
     function loadNowPlaying()
@@ -104,10 +156,76 @@ controllers.controller('StationCtrl', function($scope, $stateParams, pvlService,
         var old_stream = $scope.stream;
         var new_stream = _.find($scope.station.streams, {'id': active_stream});
 
+        if (old_stream.status && new_stream.status)
+        {
+            if (old_stream.current_song.id != new_stream.current_song.id)
+            {
+                $('#btn-like-song,#btn-dislike-song').removeClass('positive assertive');
+            }
+        }
+
         $scope.stream = new_stream;
     }
 
+    function playStream(stream)
+    {
+        if (playing_stream)
+        {
+            if (playing_stream == stream.id)
+                return true;
+            else
+                stopPlayer();
+        }
+
+        if (window.cordova)
+        {
+            audio_element = new Media(stream.url, function() {
+                console.log("playAudio(): Audio Success");
+            },
+            function(err) {
+                console.error("playAudio(): Audio Error");
+                console.error(err);
+            },
+            function(status) {
+                console.log(status);
+            });
+
+            audio_element.play();
+        }
+        else
+        {
+            if (audio_element == null)
+                audio_element = document.createElement('audio');
+
+            audio_element.setAttribute('src', stream.url);
+            audio_element.play();
+        }
+
+        playing_stream = stream.id;
+    }
+
+    function stopPlayer()
+    {
+        if (audio_element !== null)
+        {
+            if (window.cordova)
+            {
+                audio_element.stop();
+            }
+            else
+            {
+                audio_element.pause();
+                audio_element.setAttribute('src', '');
+            }
+        }
+
+        playing_stream = null;
+    }
+
+    // Unload player on page switch.
     $scope.$on("$destroy", function(event) {
+        stopPlayer();
+
         $timeout.cancel(np_timeout);
     });
 
