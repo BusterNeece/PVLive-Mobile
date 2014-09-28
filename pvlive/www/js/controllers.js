27ce0bc48fcc51f23c95abfc4ff70c28f6ea0486
Buster "Silver Eagle" Neece <buster@busterneece.com>
2014-09-28 03:46:34 -0500

    2.0.6: Fixing the "now-playing arrow" in the header, moving nowplaying/notify loop into radioService.

--- pvlive/www/js/controllers.js
+++ pvlive/www/js/controllers.js
@@ -1,6 +1,8 @@
 var controllers = angular.module('pvlive.controllers', []);
 
-controllers.controller('AppCtrl', function($scope) {});
+controllers.controller('AppCtrl', function($scope, radioService) {
+    $scope.radio = radioService;
+});
 
 /**
  * Stations Listing
@@ -51,11 +53,9 @@ controllers.controller('StationsCtrl', function($scope, apiService, loadingServi
 /**
  * Individual Station & Playback
  */
-controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams, apiService, radioService, loadingService, $timeout, $window) {
+controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams, $window, apiService, radioService, loadingService) {
 
-    var np_timeout = null;
-    var active_stream = null;
-
+    $scope.active_stream = null;
     $scope.station = {};
     $scope.stream = {};
 
@@ -65,7 +65,7 @@ controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams,
 
     // On-page conditionals.
     $scope.isActiveStream = function(stream) {
-        return (active_stream == stream.id);
+        return ($scope.active_stream == stream.id);
     };
     $scope.isPlaying = function(stream) {
         return (radioService.isStreamPlaying(stream));
@@ -74,26 +74,21 @@ controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams,
     // On page triggered events.
     $scope.playStream = function(stream)
     {
-        active_stream = stream.id;
+        $scope.active_stream = stream.id;
 
         // Special handling for video streams.
         if (stream.type == "stream")
         {
-            window.open(encodeURI(url), '_system', 'location=yes');
+            $window.open(encodeURI(url), '_system', 'location=yes');
             return;
         }
 
         processNowPlaying();
 
         if ($scope.isPlaying(stream))
-        {
             stopPlayer();
-        }
         else
-        {
             playStream(stream);
-            notifyNewSong(stream.current_song);
-        }
     };
 
     $scope.likeSong = function(song)
@@ -152,51 +147,38 @@ controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams,
             processNowPlaying();
 
             $scope.$broadcast('scroll.refreshComplete');
-
-            if (np_timeout !== null)
-                $timeout.cancel(np_timeout);
-
-            np_timeout = $timeout(loadNowPlaying, 30000);
             loadingService.hide();
         });
     }
 
+    $scope.$on('radio:updated', function(event, np) {
+        if (np.station.id == $scope.station.station.id)
+        {
+            $scope.station = np;
+            processNowPlaying();
+        }
+    });
+
     function processNowPlaying()
     {
-        if (active_stream == null)
+        if ($scope.active_stream == null)
         {
             if (radioService.stream)
             {
-                active_stream = radioService.stream.id;
+                $scope.active_stream = radioService.stream.id;
             }
             else
             {
                 var default_stream = _.find($scope.station.streams, {'is_default': true});
-                active_stream = default_stream.id;
+                $scope.active_stream = default_stream.id;
             }
         }
 
-        var old_stream = $scope.stream;
-        var new_stream = _.find($scope.station.streams, {'id': active_stream});
+        $scope.stream = _.find($scope.station.streams, {'id': $scope.active_stream});
 
-        $scope.stream = new_stream;
-
-        var song_changed;
-
-        // Detect song change.
-        if (old_stream.status && new_stream.status)
-            song_changed = (old_stream.current_song.id != new_stream.current_song.id);
-        else
-            song_changed = true;
-
-        if (song_changed)
-        {
-            document.getElementById('btn-like-song').classList.remove('positive');
-            document.getElementById('btn-dislike-song').classList.remove('assertive');
-
-            if (radioService.isPlaying())
-                notifyNewSong(new_stream.current_song);
-        }
+        // Reset like/dislike buttons.
+        document.getElementById('btn-like-song').classList.remove('positive');
+        document.getElementById('btn-dislike-song').classList.remove('assertive');
     }
 
     function playStream(stream)
@@ -208,25 +190,6 @@ controllers.controller('StationCtrl', function($scope, $rootScope, $stateParams,
     {
         radioService.stop();
     }
-
-    function notifyNewSong(song)
-    {
-        if (window.cordova)
-        {
-            window.plugin.notification.local.add({
-                id:         song.id,
-                message:    song.title+' by '+song.artist,
-                title:      $scope.station.station.name,
-                sound:      '',
-                autoCancel: true
-            });
-        }
-    }
-
-    // Unload player on page switch.
-    $scope.$on("$destroy", function(event) {
-        $timeout.cancel(np_timeout);
-    });
 
 });
 
